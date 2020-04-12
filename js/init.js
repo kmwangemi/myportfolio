0cd492817723dd8fcbe66f9669f8637a25d5dcc6
kimpetertanui <kimelipeter526@gmail.com>
2020-04-12 14:46:19 +0300

    updated about section

--- js/init.js
+++ js/init.js
@@ -515,7 +515,7 @@ function arlo_tm_animate_text(){
 	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
 	
 		animateSpan.typed({
-			strings: ["Android Developer", "UI/UX Designer", "Web Developer","WEFGWERGHJT"],
+			strings: ["Software Engineer", "UI/UX Designer", "Solution Architech","Data Analysist"],
 			loop: true,
 			startDelay: 1e3,
 			backDelay: 2e3
