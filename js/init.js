41c236ebccdc6a492e51967fa1e0ac297d4065ec
mwangemi <mwangemik@gmail.com>
2021-09-06 12:52:02 +0300

    reedit my portfolio

--- js/init.js
+++ js/init.js
@@ -508,18 +508,15 @@ function arlo_tm_totop_myhide(){
 // -------------   ANIMATE TEXT  -------------------
 // -------------------------------------------------
 
-function arlo_tm_animate_text(){
-	
+function arlo_tm_animate_text() {
 	"use strict";
-	
-	var animateSpan			= jQuery('.arlo_tm_animation_text_word');
-	
-		animateSpan.typed({
-			strings: ["Software Engineer"],
-			loop: true,
-			startDelay: 1e3,
-			backDelay: 2e3
-		});
+	var animateSpan = jQuery('.arlo_tm_animation_text_word');
+   animateSpan.typed({
+      strings: ["Frontend Software Developer"],
+      loop: true,
+      startDelay: 1e3,
+      backDelay: 2e3
+   });
 }
 
 // -----------------------------------------------------
